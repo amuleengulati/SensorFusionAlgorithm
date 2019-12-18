76423dde3d50f3087a3befd5b78b83ea6e9470cd
Shyam Bhuptani <shyambhuptani176@gmail.com>
2019-12-18 18:11:39 -0500

    Replacing name of src, removing tmp files

--- SensorFusion_src/main.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-int main(void)
-{
-	/*main code goes here
-	 this will contain all function calls*/
-	return 0;
-}
--- include/Support_Degree_Matrix.h
+++
@@ -1,6 +0,0 @@
-#ifndef Support_Degree_Matrix_h
-#define Support_Degree_Matrix_h
-
-double ** Support_Degree_Matrix(int n, int d[]);
-
-#endif
